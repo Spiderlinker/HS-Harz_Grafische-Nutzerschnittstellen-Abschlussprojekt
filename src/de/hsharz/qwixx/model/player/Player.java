f5eaf1b56bf6caa5a4a90c819a522c407554f3cd
Oliver Lindemann <lindemann.oliver@gmx.de>
2019-12-10 13:00:47 +0100

    Abstrakte Player-Klasse beinhaltet nun GameBoard und Getter

--- src/de/hsharz/qwixx/model/player/Player.java
+++ src/de/hsharz/qwixx/model/player/Player.java
@@ -1,6 +1,20 @@
 package de.hsharz.qwixx.model.player;
 
+import java.util.Objects;
+
+import de.hsharz.qwixx.model.board.GameBoard;
+
 public abstract class Player implements IPlayer {
 
+	private GameBoard board;
+
+	public Player(GameBoard board) {
+		this.board = Objects.requireNonNull(board);
+	}
+
+	@Override
+	public GameBoard getGameBoard() {
+		return board;
+	}
 
 }
