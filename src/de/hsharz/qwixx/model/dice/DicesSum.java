f403249968e14f4adf773245baad4587bfacf604
Spiderlinker <support@spiderlinker.de>
2019-12-15 19:54:45 +0100

    equals-Methode ueberschrieben

--- src/de/hsharz/qwixx/model/dice/DicesSum.java
+++ src/de/hsharz/qwixx/model/dice/DicesSum.java
@@ -1,5 +1,7 @@
 package de.hsharz.qwixx.model.dice;
 
+import java.util.Objects;
+
 public class DicesSum {
 
 	public static final DicesSum EMPTY = new DicesSum(null, -1);
@@ -18,6 +20,17 @@ public class DicesSum {
 
 	public int getSum() {
 		return this.sum;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof DicesSum) {
+			DicesSum dice = (DicesSum) obj;
+			return dice.getSum() == sum //
+					&& (color == dice.getColor() //
+							|| (color != null && color.equals(dice.getColor())));
+		}
+		return super.equals(obj);
 	}
 
 	@Override
