542ea11b9e00b13c7be812e7d0bc0463708f467f
Spiderlinker <support@spiderlinker.de>
2019-12-18 00:33:53 +0100

    Anordnung der Spielfelder bei unterschiedlicher Anzahl der Spieler

--- src/de/hsharz/qwixx/ui/GameUI.java
+++ src/de/hsharz/qwixx/ui/GameUI.java
@@ -2,6 +2,7 @@ package de.hsharz.qwixx.ui;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 
 import de.hsharz.qwixx.model.Game;
@@ -9,7 +10,12 @@ import de.hsharz.qwixx.model.player.Human;
 import de.hsharz.qwixx.model.player.IPlayer;
 import de.hsharz.qwixx.ui.board.GameBoardUI;
 import de.hsharz.qwixx.ui.dice.DicePane;
+import javafx.geometry.HPos;
+import javafx.geometry.VPos;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
 
 public class GameUI extends AbstractPane<GridPane> {
 
@@ -28,7 +34,13 @@ public class GameUI extends AbstractPane<GridPane> {
 
 	private void createWidgets() {
 		createPlayerBoards();
+
+		root.setHgap(10);
+		root.setVgap(5);
+		root.setGridLinesVisible(true);
+
 		dicePane = new DicePane(game.getDices());
+		game.addDiceListener(dicePane);
 	}
 
 	private void createPlayerBoards() {
@@ -45,6 +57,99 @@ public class GameUI extends AbstractPane<GridPane> {
 	}
 
 	private void addWidgets() {
+
+		boards.values().forEach(board -> {
+			board.getPane().setScaleX(0.7);
+			board.getPane().setScaleY(0.7);
+		});
+
+		root.add(dicePane.getPane(), 1, 1);
+
+		GridPane.setHalignment(dicePane.getPane(), HPos.CENTER);
+		GridPane.setValignment(dicePane.getPane(), VPos.CENTER);
+
+		switch (boards.size()) {
+
+		case 2:
+			// Boards oben mittig und unten
+			for (Entry<IPlayer, GameBoardUI> e : boards.entrySet()) {
+				Group group = new Group(e.getValue().getPane());
+				if (e.getKey() instanceof Human) {
+					root.add(group, 1, 2);
+					GridPane.setHalignment(group, HPos.CENTER);
+				} else {
+					root.add(group, 1, 0);
+					GridPane.setHalignment(group, HPos.CENTER);
+				}
+			}
+			break;
+		case 3:
+			// Boards links, rechts und unten
+			int index = 0;
+			for (Entry<IPlayer, GameBoardUI> e : boards.entrySet()) {
+				Group group = new Group(e.getValue().getPane());
+				if (e.getKey() instanceof Human) {
+					root.add(group, 1, 2);
+					GridPane.setHalignment(group, HPos.CENTER);
+				} else {
+					if (index == 0) {
+						root.add(group, 0, 1);
+					} else {
+						root.add(group, 2, 1);
+					}
+					index++;
+				}
+			}
+			break;
+		case 4:
+			// Boards oben mittig, links, rechts und unten
+			index = 0;
+			for (Entry<IPlayer, GameBoardUI> e : boards.entrySet()) {
+				Group group = new Group(e.getValue().getPane());
+				if (e.getKey() instanceof Human) {
+					root.add(group, 1, 2);
+					GridPane.setHalignment(group, HPos.CENTER);
+				} else {
+					if (index == 0) {
+						root.add(group, 0, 1);
+					} else if (index == 1) {
+						root.add(group, 1, 0);
+						GridPane.setHalignment(group, HPos.CENTER);
+					} else {
+						root.add(group, 2, 1);
+					}
+					index++;
+				}
+			}
+			break;
+		case 5:
+			// Boards oben links, oben rechts, links, rechts und unten
+			index = 0;
+			for (Entry<IPlayer, GameBoardUI> e : boards.entrySet()) {
+				Group group = new Group(e.getValue().getPane());
+				if (e.getKey() instanceof Human) {
+					root.add(group, 1, 2);
+					GridPane.setHalignment(group, HPos.CENTER);
+				} else {
+					if (index == 0) {
+						root.add(group, 0, 1);
+					} else if (index == 1) {
+						root.add(group, 0, 0, 2, 1);
+						GridPane.setHalignment(group, HPos.CENTER);
+					} else if (index == 2) {
+						root.add(group, 1, 0, 2, 1);
+						GridPane.setHalignment(group, HPos.CENTER);
+					} else {
+						root.add(group, 2, 1);
+					}
+					index++;
+				}
+			}
+			break;
+		default:
+			throw new IllegalArgumentException(
+					"Ungültige Anzahl an Spielbrettern! Min: 2; Max: 5; Current: " + boards.size());
+		}
 
 	}
 
