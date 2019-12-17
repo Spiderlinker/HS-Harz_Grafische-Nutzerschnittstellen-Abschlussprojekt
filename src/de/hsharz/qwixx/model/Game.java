9f63d38999e3affbbbb71e872dfb7be7d24ed674
Oliver Lindemann <lindemann.oliver@gmx.de>
2019-12-17 21:31:38 +0100

    Methode getWinningPlayer erstellt

--- src/de/hsharz/qwixx/model/Game.java
+++ src/de/hsharz/qwixx/model/Game.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.OptionalInt;
 import java.util.Queue;
 import java.util.Set;
 
@@ -14,6 +15,7 @@ import de.hsharz.qwixx.model.dice.DiceColor;
 import de.hsharz.qwixx.model.dice.DicesSum;
 import de.hsharz.qwixx.model.dice.IDice;
 import de.hsharz.qwixx.model.player.IPlayer;
+import de.hsharz.qwixx.ui.GameListener;
 import de.hsharz.qwixx.ui.dice.DiceListener;
 
 public class Game implements RowsClosedSupplier {
@@ -32,7 +34,8 @@ public class Game implements RowsClosedSupplier {
 	private Set<DiceColor> closedRows = new HashSet<>();
 	private Queue<DiceColor> rowsToCloseAfterRoundFinished = new ArrayDeque<>();
 
-	private List<DiceListener> listeners = new ArrayList<>();
+	private List<DiceListener> diceListeners = new ArrayList<>();
+	private List<GameListener> gameListeners = new ArrayList<>();
 
 	private boolean isPlaying;
 
@@ -54,7 +57,11 @@ public class Game implements RowsClosedSupplier {
 	}
 
 	public void addDiceListener(DiceListener l) {
-		this.listeners.add(l);
+		this.diceListeners.add(l);
+	}
+
+	public void addGameListener(GameListener l) {
+		this.gameListeners.add(l);
 	}
 
 	private void addDices() {
@@ -94,7 +101,8 @@ public class Game implements RowsClosedSupplier {
 			for (IPlayer currentPlayer : this.player) {
 
 				System.out.println("\n\n Spieler ist an der Reihe: " + currentPlayer);
-				
+				gameListeners.forEach(l -> l.nextPlayersTurn(currentPlayer));
+
 				rollDices();
 				List<DicesSum> whiteDices = getWhiteDicesSums();
 				List<DicesSum> colorDices = getColorDicesSums();
@@ -105,8 +113,18 @@ public class Game implements RowsClosedSupplier {
 				System.out.println("---------- Current Player choosing dices");
 				DicesSum selectedWhiteDice = currentPlayer.chooseWhiteDices(whiteDices);
 				if (!selectedWhiteDice.equals(DicesSum.EMPTY)) {
-					if(whiteDices.contains(selectedWhiteDice)) {
-						System.out.println("###################### DiceSum not in list, Schummler!: " + selectedWhiteDice + " - " + whiteDices);
+
+					OptionalInt findFirst = whiteDices.stream().mapToInt(DicesSum::getSum)
+							.filter(i -> i == selectedWhiteDice.getSum()).findFirst();
+
+					if (!findFirst.isPresent()) {
+						System.out.println("###################### DiceSum not in list, Schummler!: "
+								+ selectedWhiteDice + " - " + whiteDices);
+
+						for (DicesSum sum : whiteDices) {
+							System.out.println(sum.equals(selectedWhiteDice));
+						}
+
 					}
 					currentPlayer.getGameBoard().crossField(selectedWhiteDice.getColor(), selectedWhiteDice.getSum());
 				}
@@ -119,8 +137,13 @@ public class Game implements RowsClosedSupplier {
 
 				DicesSum selectedColorDice = currentPlayer.chooseColorDices(colorDices);
 				if (!selectedColorDice.equals(DicesSum.EMPTY)) {
-					if(colorDices.contains(selectedWhiteDice)) {
-						System.out.println("###################### DiceSum not in list, Schummler!: " + selectedColorDice + " - " + colorDices);
+					if (!colorDices.contains(selectedColorDice)) {
+						System.out.println("###################### DiceSum not in list, Schummler!: "
+								+ selectedColorDice + " - " + colorDices);
+
+						for (DicesSum sum : colorDices) {
+							System.out.println(sum.equals(selectedColorDice));
+						}
 					}
 					currentPlayer.getGameBoard().crossField(selectedColorDice.getColor(), selectedColorDice.getSum());
 				}
@@ -139,14 +162,13 @@ public class Game implements RowsClosedSupplier {
 			}
 		}
 		System.out.println("Game over");
-
 	}
 
 	private void rollDices() {
 		dices.forEach(IDice::rollDice);
 		dices.forEach(System.out::println);
 
-		listeners.forEach(DiceListener::dicesRolled);
+		diceListeners.forEach(DiceListener::dicesRolled);
 	}
 
 	private List<DicesSum> getWhiteDicesSums() {
@@ -170,14 +192,12 @@ public class Game implements RowsClosedSupplier {
 	private void letOtherPlayerChooseWhiteDices(List<DicesSum> whiteDices, IPlayer currentPlayer) {
 
 		player.forEach(p -> {
-//			new Thread(() -> {
 			if (!p.equals(currentPlayer)) {
 				DicesSum selectedDices = p.chooseWhiteDices(whiteDices);
 				if (!selectedDices.equals(DicesSum.EMPTY)) {
 					p.getGameBoard().crossField(selectedDices.getColor(), selectedDices.getSum());
 				}
 			}
-//			}).start();
 		});
 	}
 
@@ -199,6 +219,16 @@ public class Game implements RowsClosedSupplier {
 			}
 		}
 		return false;
+	}
+
+	public IPlayer getWinningPlayer() {
+		return player.stream().reduce(this::getPlayerWithMaxScore).orElseGet(null);
+	}
+
+	private IPlayer getPlayerWithMaxScore(IPlayer p1, IPlayer p2) {
+		return p1.getGameBoard().getScore().getScoreComplete() > p2.getGameBoard().getScore().getScoreComplete() //
+				? p1
+				: p2;
 	}
 
 	@Override
