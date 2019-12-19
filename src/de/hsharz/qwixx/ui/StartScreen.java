f03c3088726d486b82b83b3196df90f19132acaa
Spiderlinker <support@spiderlinker.de>
2019-12-19 12:41:43 +0100

    StartScreen erstellt

---
+++ src/de/hsharz/qwixx/ui/StartScreen.java
@@ -0,0 +1,112 @@
+package de.hsharz.qwixx.ui;
+
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
+
+public class StartScreen extends AbstractPane<VBox> {
+
+	private Label lblQwixx;
+
+	private Separator separator;
+
+	private Button btnPlay;
+	private Button btnStatistics;
+	private Button btnExit;
+
+	private StartGameScreen startGameScreen;
+
+	public StartScreen() {
+		super(new VBox());
+
+		createWidgets();
+		setupInteractions();
+		addWidgets();
+
+	}
+
+	private void createWidgets() {
+		root.setPadding(new Insets(50));
+		root.setSpacing(20);
+		root.setAlignment(Pos.CENTER);
+		root.setStyle("-fx-background-color: white;");
+
+		lblQwixx = new Label("Qwixx");
+		lblQwixx.setStyle("-fx-font-size: 40pt;");
+		lblQwixx.setAlignment(Pos.CENTER);
+
+		separator = new Separator(Orientation.HORIZONTAL);
+
+		btnPlay = new Button("Spiel starten");
+		btnPlay.setStyle("-fx-font-size: 16pt;");
+		btnPlay.setMaxWidth(Double.MAX_VALUE);
+
+		btnStatistics = new Button("Statistik");
+		btnStatistics.setStyle("-fx-font-size: 16pt;");
+		btnStatistics.setMaxWidth(Double.MAX_VALUE);
+
+		btnExit = new Button("Verlassen");
+		btnExit.setStyle("-fx-font-size: 16pt;");
+		btnExit.setMaxWidth(Double.MAX_VALUE);
+	}
+
+	private void setupInteractions() {
+		btnPlay.setOnAction(e -> showStartGameScreen());
+		btnExit.setOnAction(e -> Platform.exit());
+	}
+
+	private void addWidgets() {
+		root.getChildren().add(lblQwixx);
+		root.getChildren().add(separator);
+		root.getChildren().add(getSpacer());
+		root.getChildren().add(btnPlay);
+		root.getChildren().add(btnStatistics);
+		root.getChildren().add(getSpacer());
+		root.getChildren().add(btnExit);
+
+		VBox.setVgrow(separator, Priority.ALWAYS);
+	}
+
+	private Node getSpacer() {
+		Region spacer = new Region();
+		VBox.setVgrow(spacer, Priority.ALWAYS);
+		return spacer;
+	}
+
+	private void showStartGameScreen() {
+		if (startGameScreen == null) {
+			startGameScreen = new StartGameScreen(getPane());
+		}
+
+		Scene scene = getPane().getScene();
+		VBox paneToShow = startGameScreen.getPane();
+
+		paneToShow.translateXProperty().set(scene.getWidth());
+
+		StackPane parentContainer = (StackPane) scene.getRoot();
+		parentContainer.getChildren().add(paneToShow);
+
+		Timeline timeline = new Timeline();
+		KeyValue keyValue = new KeyValue(paneToShow.translateXProperty(), 0, Interpolator.EASE_IN);
+		KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.3), keyValue);
+		timeline.getKeyFrames().add(keyFrame);
+		timeline.setOnFinished(e -> parentContainer.getChildren().remove(getPane()));
+		timeline.play();
+	}
+
+}
