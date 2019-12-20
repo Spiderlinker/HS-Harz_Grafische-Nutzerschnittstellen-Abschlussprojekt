32bb5c61d82ba539fcfa8bde1bb58acb19f2dbc1
Oliver Lindemann <lindemann.oliver@gmx.de>
2019-12-20 21:35:35 +0100

    StartScreen als Einstiegspunkt in Applikation

--- src/de/hsharz/qwixx/Qwixx.java
+++ src/de/hsharz/qwixx/Qwixx.java
@@ -1,13 +1,8 @@
 package de.hsharz.qwixx;
 
-import de.hsharz.qwixx.model.Game;
-import de.hsharz.qwixx.model.board.GameBoard;
-import de.hsharz.qwixx.model.player.Computer;
-import de.hsharz.qwixx.model.player.Human;
-import de.hsharz.qwixx.model.player.IPlayer;
 import de.hsharz.qwixx.ui.StartScreen;
-import de.hsharz.qwixx.ui.game.GameUI;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -20,41 +15,16 @@ public class Qwixx extends Application {
 
 //		root.setStyle("-fx-background-color: black;");
 
-		Game game = new Game();
-
-		{
-			GameBoard board = new GameBoard();
-			board.setRowClosedSupplier(game);
-			IPlayer player = new Human("Peter", board);
-			game.addPlayer(player);
-		}
-
-		for (int i = 0; i < 4; i++) {
-			GameBoard board = new GameBoard();
-			board.setRowClosedSupplier(game);
-
-			IPlayer player = new Computer("Computer #" + i, board);
-			game.addPlayer(player);
-		}
-
-		GameUI gameUI = new GameUI(game);
-//		root.setCenter(gameUI.getPane());
-
-		new Thread(game::startGame).start();
-
 //		Background background = new Background(new BackgroundImage(new Image(new File("images/background.jpg").toURI().toString()),
 //				BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER,
 //				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)));
 //		root.setBackground(background);
 
-		
-		root.getChildren().add(new StartScreen().getPane());
-		
-		primaryStage.setOnCloseRequest(e -> System.exit(0));
-		primaryStage.setTitle("Hello World");
-		primaryStage.setScene(new Scene(root, 400, 500));
-//		primaryStage.setFullScreen(true);
+		root.getChildren().add(new StartScreen(primaryStage).getPane());
 
+		primaryStage.setOnCloseRequest(e -> Platform.exit());
+		primaryStage.setTitle("Qwixx");
+		primaryStage.setScene(new Scene(root, 440, 500));
 		primaryStage.show();
 
 	}
