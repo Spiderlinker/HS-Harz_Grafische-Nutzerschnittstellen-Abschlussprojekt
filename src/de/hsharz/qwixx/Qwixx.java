876beadb96b24686ffa615552c95718a3bdbebd5
Spiderlinker <support@spiderlinker.de>
2020-01-08 22:39:28 +0100

    Beenden verbessert

--- src/de/hsharz/qwixx/Qwixx.java
+++ src/de/hsharz/qwixx/Qwixx.java
@@ -2,7 +2,6 @@ package de.hsharz.qwixx;
 
 import de.hsharz.qwixx.ui.StartScreen;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -12,17 +11,12 @@ public class Qwixx extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		StackPane root = new StackPane();
-
-//		root.setStyle("-fx-background-color: black;");
-
-//		Background background = new Background(new BackgroundImage(new Image(new File("images/background.jpg").toURI().toString()),
-//				BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER,
-//				new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true)));
-//		root.setBackground(background);
-
 		root.getChildren().add(new StartScreen(primaryStage).getPane());
 
-		primaryStage.setOnCloseRequest(e -> Platform.exit());
+		primaryStage.setOnCloseRequest(e -> {
+			primaryStage.hide();
+			System.exit(0);
+		});
 		primaryStage.setTitle("Qwixx");
 		primaryStage.setScene(new Scene(root, 440, 500));
 		primaryStage.show();
