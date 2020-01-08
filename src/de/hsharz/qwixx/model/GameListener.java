d2a854b169545ddbf3f02b82992ddc0278bfe4db
Spiderlinker <support@spiderlinker.de>
2020-01-08 22:39:08 +0100

    Neue Methode hinzugefuegt

--- src/de/hsharz/qwixx/model/GameListener.java
+++ src/de/hsharz/qwixx/model/GameListener.java
@@ -1,10 +1,12 @@
-package de.hsharz.qwixx.ui.game;
+package de.hsharz.qwixx.model;
 
 import de.hsharz.qwixx.model.player.IPlayer;
 
 public interface GameListener {
 
 	void nextPlayersTurn(IPlayer nextPlayer);
-	
+
+	void invalidDiceChoiceMade(IPlayer player, String msg);
+
 	void gameOver();
 }
