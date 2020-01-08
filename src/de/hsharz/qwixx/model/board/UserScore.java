3a18af78ed50f579bb642e0d35bed5feea847483
Spiderlinker <support@spiderlinker.de>
2020-01-08 22:39:49 +0100

    Reihen als Liste implementiert

--- src/de/hsharz/qwixx/model/board/UserScore.java
+++ src/de/hsharz/qwixx/model/board/UserScore.java
@@ -1,7 +1,11 @@
 package de.hsharz.qwixx.model.board;
 
 import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+
+import de.hsharz.qwixx.model.dice.DiceColor;
 
 public class UserScore {
 
@@ -9,50 +13,27 @@ public class UserScore {
 
 	private static List<Integer> score = Arrays.asList(0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78);
 
-	private int scoreRedRow;
-	private int scoreYellowRow;
-	private int scoreGreenRow;
-	private int scoreBlueRow;
+	private Map<DiceColor, Integer> rowScores = new EnumMap<>(DiceColor.class);
 	private int scoreMisses;
 
 	public UserScore() {
-
+		rowScores.put(DiceColor.RED, 0);
+		rowScores.put(DiceColor.YELLOW, 0);
+		rowScores.put(DiceColor.GREEN, 0);
+		rowScores.put(DiceColor.BLUE, 0);
 	}
 
-	public void setScoreRedRow(int amountRedFieldsCrossed) {
-		this.scoreRedRow = score.get(amountRedFieldsCrossed);
-	}
-
-	public void setScoreYellowRow(int amountYellowFieldsCrossed) {
-		this.scoreYellowRow = score.get(amountYellowFieldsCrossed);
-	}
-
-	public void setScoreGreenRow(int amountGreenFieldsCrossed) {
-		this.scoreGreenRow = score.get(amountGreenFieldsCrossed);
-	}
-
-	public void setScoreBlueRow(int amountBlueFieldsCrossed) {
-		this.scoreBlueRow = score.get(amountBlueFieldsCrossed);
+	public void setScoreOfRow(DiceColor rowColor, int amountRedFieldsCrossed) {
+		rowScores.put(rowColor, score.get(amountRedFieldsCrossed));
 	}
 
 	public void setScoreMisses(int amountMisses) {
 		this.scoreMisses = SCORE_PER_MISS * amountMisses;
 	}
 
-	public int getScoreRedRow() {
-		return scoreRedRow;
-	}
-
-	public int getScoreYellowRow() {
-		return scoreYellowRow;
-	}
-
-	public int getScoreGreenRow() {
-		return scoreGreenRow;
-	}
-
-	public int getScoreBlueRow() {
-		return scoreBlueRow;
+	public int getScoreOfRow(DiceColor rowColor) {
+		Integer scoreOfRow = rowScores.get(rowColor);
+		return scoreOfRow == null ? 0 : scoreOfRow;
 	}
 
 	public int getScoreMisses() {
@@ -60,10 +41,9 @@ public class UserScore {
 	}
 
 	public int getScoreComplete() {
-		return getScoreRedRow() //
-				+ getScoreYellowRow() //
-				+ getScoreGreenRow() //
-				+ getScoreBlueRow() //
+		return rowScores.values().stream()//
+				.mapToInt(i -> i) //
+				.sum() //
 				+ getScoreMisses();
 	}
 
