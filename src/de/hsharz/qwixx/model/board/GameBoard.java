8c9f979c1f40e87d0941c34ce50a2fed3d718a8d
Oliver Lindemann <lindemann.oliver@gmx.de>
2020-01-12 09:19:28 +0100

    JavaDoc hinzugefügt

--- src/de/hsharz/qwixx/model/board/GameBoard.java
+++ src/de/hsharz/qwixx/model/board/GameBoard.java
@@ -13,64 +13,154 @@ import de.hsharz.qwixx.model.board.row.field.Field;
 import de.hsharz.qwixx.model.dice.DiceColor;
 import de.hsharz.qwixx.model.dice.DicePair;
 
+/**
+ * Diese Klasse repräsentieirt das Spielfeld bzw. den Spielblock eines einzelnen
+ * Spielers. <br>
+ * Ein Spielblock beinhaltet 4 Reihen ({@link Row}), die Anzahl der
+ * verbleibenden Fehlwürfe und den Score.
+ * <p>
+ * Das GameBoard stellt zudem Methoden zur Registrierung von GameBoardListener
+ * für dieses GameBoard zur Verfügung. Die über diese Methoden registrierten
+ * Listener werden über angekreuzte Felder, abgeschlossene Reihen und
+ * angekreuzte Fehlwürfel benachrichtigt.
+ * 
+ * @author Oliver Lindemann
+ */
 public class GameBoard {
 
-	private static final int MIN_FIELDS_CROSSED_TO_CLOSE_ROW = 6;
-	private static final int AMOUNT_MISSES = 4;
+	/**
+	 * Mindestanzahl der angekreuzten Felder in einer Reihe, ab der erst das
+	 * Zusatzfeld (Schloss) in einer Reihe angekreuzt und somit die Reihe
+	 * abgeschlossen werden darf
+	 */
+	public static final int MIN_FIELDS_CROSSED_TO_CLOSE_ROW = 6;
 
+	/** Anzahl der möglichen Fehlwürfe */
+	public static final int AMOUNT_MISSES = 4;
+
+	/**
+	 * Supplier über den Reihen abgeschlossen werden können bzw. dies geprüft werden
+	 * kann
+	 */
 	private RowsClosedSupplier rowClosedSupplier;
+	/** Map mit Reihen auf diesem Spielfeld (<Reihenfarbe, Reihe>) */
 	private Map<DiceColor, Row> rows = new EnumMap<>(DiceColor.class);
 
+	/** Anzahl der verbleibenden Fehlwürfe */
 	private int remainingMisses = AMOUNT_MISSES;
-
-	private List<GameBoardListener> listeners = new ArrayList<>();
-
+	/** Punktezahl dieses Spielfeldes */
 	private UserScore userScore;
 
+	/** Liste mit registrierten GameBoardListener */
+	private List<GameBoardListener> listeners = new ArrayList<>();
+
+	/**
+	 * Erzeugt ein neues Spielfeld.
+	 * <p>
+	 * Damit dieses Spielfeld verwendet werden kann, muss noch über die Methode
+	 * {@link #setRowClosedSupplier(RowsClosedSupplier)} ein
+	 * {@link RowsClosedSupplier} gesetzt werden. Ansonsten kann es zu einem
+	 * fehlerhaften Verhalten bzw. Fehlermeldungen führen.
+	 */
 	public GameBoard() {
 		userScore = new UserScore();
-
 		createRows();
 	}
 
 	private void createRows() {
+		// Für die Farben RED, YELLOW, GREEN und BLUE Reihen erstellen
 		rows.put(DiceColor.RED, new Row(DiceColor.RED, Order.ASC));
 		rows.put(DiceColor.YELLOW, new Row(DiceColor.YELLOW, Order.ASC));
 		rows.put(DiceColor.GREEN, new Row(DiceColor.GREEN, Order.DESC));
 		rows.put(DiceColor.BLUE, new Row(DiceColor.BLUE, Order.DESC));
 	}
 
+	/**
+	 * Der gegebene {@link GameBoardListener} wird auf dieses Spielfeld registriert
+	 * und wird zukünftig über alle Veränderungen auf diesem Spielfeld
+	 * benachrichtigt.
+	 * 
+	 * @param l GameBoardListener der auf dieses Spielfeld registriert werden soll
+	 */
 	public void addListener(GameBoardListener l) {
 		this.listeners.add(l);
 	}
 
+	/**
+	 * Der gegebene {@link GameBoardListener} wird von den auf diesem Spielfeld
+	 * registrierte Listener entfernt und zukünftig nicht weiter über Veränderungen
+	 * auf diesem Spielfeld benachrichtigt.
+	 * 
+	 * @param l GameBoardListener der nicht länger auf dieses Spielfeld registriert
+	 *          sein soll
+	 */
 	public void removeListener(GameBoardListener l) {
 		this.listeners.remove(l);
 	}
 
+	/**
+	 * Setzt den {@link RowsClosedSupplier} dieses Spielfeldes. <b> Falls der
+	 * gegebene {@code supplier} null sein sollte, so wird jeglicher zuvor
+	 * registrierte {@link RowsClosedSupplier} von diesem Spielfeld entfernt.
+	 * <p>
+	 * Beachte, dass ein {@link RowsClosedSupplier} für ein funktionierendes
+	 * Spielfeld benötigt ist.
+	 * 
+	 * @param supplier Supplier zum Abschließen von Reihen bzw. der Abfrage über
+	 *                 geschlossene Reihen
+	 */
 	public void setRowClosedSupplier(RowsClosedSupplier supplier) {
 		this.rowClosedSupplier = supplier;
 	}
 
+	/**
+	 * Liefert die Reihen dieses Spielfeldes. Die Reihen sind in eienr Map
+	 * organisiert, die der Logik <Reihenfarbe, Reihe> folgt.
+	 * 
+	 * @return Liefert alle Reihen dieses Spielfeldes
+	 */
 	public Map<DiceColor, Row> getRows() {
 		return rows;
 	}
 
+	/**
+	 * Liefert die passende Reihe ({@link Row}) zu der gegebenen Reihenfarbe
+	 * {@code color}. <br>
+	 * Falls es keine Reihe mit der gegebenen Farbe {@code color} gibt, so wird
+	 * {@code null} zurückgeliefert.
+	 * 
+	 * @param color Farbe der gesuchten Reihe
+	 * @return Reihe mit der gegebenen Farbe {@code color} oder null, falls keine
+	 *         Reihe gefunden werden konnte
+	 */
 	public Row getRow(DiceColor color) {
 		return rows.get(color);
 	}
 
+	/**
+	 * @return Liefert die Anzahl der verbleibenden Fehlwürfe
+	 *         ({@link #AMOUNT_MISSES} - bereits verwendete Fehlwürfe)
+	 */
 	public int getRemainingMisses() {
 		return remainingMisses;
 	}
 
+	/**
+	 * Kreuzt einen Fehlwurf auf diesem Spielfeld an. <br>
+	 * Falls es keinen verbleibenden Fehlwurf mehr gibt, so wird eine
+	 * {@link IllegalStateException} geworfen.
+	 * 
+	 * <p>
+	 * Nach Verringern der verbleibenden Fehlwürfe werden alle registrierten
+	 * {@link GameBoardListener} über diesen Vorgang benachrichtigt.
+	 */
 	public void crossMiss() {
 		if (remainingMisses <= 0) {
-			throw new IllegalAccessError("No remaining misses to cross! Remaining: " + remainingMisses);
+			throw new IllegalStateException("No remaining misses to cross! Remaining: " + remainingMisses);
 		}
 
 		remainingMisses--;
-		updateScore();
+		updateScore(); // Score aktualisieren (-5 Punkte für jeden Fehlwurf)
 
 		for (GameBoardListener l : listeners) {
 			l.missCrossed();
@@ -149,17 +239,18 @@ public class GameBoard {
 	}
 
 	private void checkIfCrossedFieldWasLastInRow(Row rowOfField, Field crossedField) {
+		/*
+		 * Falls das angekreuzte Feld das letzte Feld in der Reihe war und genügend
+		 * Felder in der Reihe angekreuzt sind, so wird auch das Zusatzfeld angekreuzt
+		 */
+		if (isLastFieldOfRow(crossedField, rowOfField) && areEnoughFieldsCrossed(rowOfField)) {
 
-		if (!areEnoughFieldsCrossed(rowOfField)) {
-			return;
-		}
-
-		if ((rowOfField.getOrder().equals(Order.ASC) && crossedField.getValue() == Row.ASC_LAST_VALUE)
-				|| rowOfField.getOrder().equals(Order.DESC) && crossedField.getValue() == Row.DESC_LAST_VALUE) {
-
-			rowOfField.getFields().get(rowOfField.getFields().size() - 1).setCrossed(true); // cross last field
+			// Zusatzfeld ankreuzen
+			rowOfField.getFields().get(rowOfField.getFields().size() - 1).setCrossed(true);
+			// Reihe schließen
 			rowClosedSupplier.closeRow(rowOfField.getColor());
 
+			// Listener benachrichtigen
 			for (GameBoardListener l : listeners) {
 				l.rowFinished(rowOfField.getColor());
 			}
@@ -167,26 +258,72 @@ public class GameBoard {
 
 	}
 
+	/**
+	 * Liefert einen {@link Boolean} der angibt, ob in der gegebenen Reihe
+	 * {@code rowToCheck} bereits genügend Felder angekreuzt sind, um eine Reihe
+	 * abzuschließen.<br>
+	 * Falls bereits {@value #MIN_FIELDS_CROSSED_TO_CLOSE_ROW} oder mehr Felder
+	 * angekreuzt sind, so liefert diese Methode {@code true}; andernfalls
+	 * {@code false}.
+	 * 
+	 * @param rowToCheck Reihe, dessen Anzahl an angekreuzten Feldern geprüft werden
+	 *                   soll
+	 * @return true, falls {@value #MIN_FIELDS_CROSSED_TO_CLOSE_ROW} oder mehr
+	 *         Felder in der gegebenen Reihe {@code rowToCheck} angekreuzt sind;
+	 *         andernfalls false
+	 */
 	private boolean areEnoughFieldsCrossed(Row rowToCheck) {
 		return rowToCheck.getFields().stream() //
 				.filter(Field::isCrossed) //
 				.count() >= MIN_FIELDS_CROSSED_TO_CLOSE_ROW;
 	}
 
+	/**
+	 * Liefert einen {@link Boolean} der angibt, ob das gegebene Feld {@code field}
+	 * das letzte Feld in der gegebenen Reihe {@code row} ist.
+	 * 
+	 * @param field Feld, welches untersucht werden soll
+	 * @param row   Reihe des Feldes {@code field}
+	 * @return true, falls das gegebene Feld {@code field} das letzte Feld in der
+	 *         Reihe {@code row} ist; false falls nicht
+	 */
+	private boolean isLastFieldOfRow(Field field, Row row) {
+		return (row.getOrder().equals(Order.ASC) && field.getValue() == Row.ASC_LAST_VALUE)
+				|| row.getOrder().equals(Order.DESC) && field.getValue() == Row.DESC_LAST_VALUE;
+	}
+
+	/**
+	 * Aktualisiert den Score dieses Spielfeldes. <br>
+	 * Hierbei wird der Score jeder Reihe und der Fehlwürfe aktualisiert.
+	 */
 	private void updateScore() {
 		// Update score field of all rows
 		rows.entrySet().forEach(e -> userScore.setScoreOfRow(e.getKey(), getAmountCrossedFieldsOfRow(e.getValue())));
 		userScore.setScoreMisses(AMOUNT_MISSES - remainingMisses);
 	}
 
+	/**
+	 * Zählt die Anzahl der angekreuzten Felder in der gegebenen Reihe {@code row}
+	 * und liefert diesen Wert zurück.
+	 * 
+	 * @param row Reihe, in der die angekreuzten Felder gezählt werden sollen
+	 * @return Anzahl der angekreuzten Felder in der gegebenen Reihe {@code row}
+	 */
 	private int getAmountCrossedFieldsOfRow(Row row) {
 		return (int) row.getFields().stream().filter(Field::isCrossed).count();
 	}
 
+	/**
+	 * @return Liefert die {@link RowsClosedSupplier}, der diesem Spielfeld
+	 *         zugewiesen wurde
+	 */
 	public RowsClosedSupplier getRowClosedSupplier() {
 		return rowClosedSupplier;
 	}
 
+	/**
+	 * @return Liefert den Score dieses Spielfeldes
+	 */
 	public UserScore getScore() {
 		return userScore;
 	}
