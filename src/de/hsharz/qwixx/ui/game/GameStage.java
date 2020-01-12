1dc8a9c6d0aba3c4cb68813dfa00a80788c6dcbd
Oliver Lindemann <lindemann.oliver@gmx.de>
2020-01-12 09:35:07 +0100

    Steuerung und Hilfedialog erstellt

--- src/de/hsharz/qwixx/ui/game/GameStage.java
+++ src/de/hsharz/qwixx/ui/game/GameStage.java
@@ -3,6 +3,9 @@ package de.hsharz.qwixx.ui.game;
 import java.util.Objects;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialog.DialogTransition;
+import com.jfoenix.controls.JFXToggleButton;
 
 import de.hsharz.qwixx.model.Game;
 import de.hsharz.qwixx.model.GameListener;
@@ -19,11 +22,14 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyCombination;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -38,7 +44,9 @@ public class GameStage extends AbstractPane<StackPane> implements GameListener {
 
 	private HBox boxButtons;
 	private JFXButton btnExit;
+	private JFXButton btnControls;
 	private JFXButton btnHelp;
+	private JFXToggleButton btnShowNotifications;
 
 	private GameOverDialog dialogGameOver;
 	private Notification notification;
@@ -79,6 +87,12 @@ public class GameStage extends AbstractPane<StackPane> implements GameListener {
 		btnExit.setStyle("-fx-background-color: white;");
 		btnHelp = new JFXButton("Hilfe");
 		btnHelp.setStyle("-fx-background-color: white;");
+		btnControls = new JFXButton("Steuerung");
+		btnControls.setStyle("-fx-background-color: white;");
+
+		btnShowNotifications = new JFXToggleButton();
+		btnShowNotifications.setText("Hinweise anzeigen");
+		btnShowNotifications.setSelected(true);
 
 		notification = new Notification();
 		dialogGameOver = new GameOverDialog(getPane());
@@ -87,7 +101,109 @@ public class GameStage extends AbstractPane<StackPane> implements GameListener {
 	private void setupInteractions() {
 		stage.addEventHandler(KeyEvent.KEY_PRESSED, e -> showExitGameScreen());
 		btnExit.setOnAction(e -> showExitGameScreen());
+		btnHelp.setOnAction(e -> {
+
+			TextFlow text = new TextFlow();
+			text.setPadding(new Insets(50));
+			text.setMaxWidth(stage.getWidth() / 1.5);
+
+			Text textGoalHeader = getBoldText("Spielziel\n");
+			Text textGoal = getNormalText(
+					"Ziel des Spiels ist es, über mehrere Runden verteilt so viele Kreuze wie möglich in den vier Zahlenreihen des eigenen Blocks einzutragen und so die meisten Punkte zu sammeln.\n\n");
+
+			Text textAblaufHeader = getBoldText("Spielablauf\n");
+			Text textAblauf = getNormalText(
+					"Wer am Zug ist und somit der aktive Spieler ist, der wirfst mit alle Würfeln genau einmal. Alle Spieler dürfen (nicht müssen!) nun die Summe der geworfenen weißen Würfeln in einer beliebigen Farbreihe auf Ihrem Spielblock ankreuzen.\n"
+							+ "Der aktive Spieler  darf nun zusätzlich die Augenzahl eines beliebigen weißen Würfels mit der Augenzahl eines Farbwürfels addieren und die Summe in die Farbreihe (entsprechend des gewählten Farbwürfels) eintragen.\n\n"
+							+ "Falls ein Spieler keine Zahl akreuzen will oder kann, so muss er ein Kreuz in der Spalte \"Fehlwürfe\" machen. Für nicht aktive Spieler gilt diese Regel nicht.\n\n");
+
+			Text textHintHeader = getBoldText("Hinweise zum Abkreuzen von Feldern\n");
+			Text textHint = getNormalText(
+					"– Alle Zahlen in jeder Reihe müssen von links nach rechts angekreuzt werden.\n"
+							+ "– Es dürfen Zahlen ausgelassen werden.\n"
+							+ "– Ausgelassene Ziffern dürfen nicht nachträglich angekreuzt werden.\n"
+							+ "- Um eine Reihe beenden zu können (und somit das Schloss am Ende dieser Reihe anzukreuzen), müssen in der Reihe mindestens 6 Kreuze gesetzt worden sein, wobei eines hiervon die letze Ziffer in der Reihe sein muss (12 oder 2)."
+							+ "Falls ein Spieler mit weniger als 6 Kreuzen die letzte Ziffer in der Reihe ankreuzt, so wird die Reihe von ihm nicht beendet und andere Spieler können weiterhin in dieser Reihe Kreuze setzen.\n\n");
+
+			Text textFinishHeader = getBoldText("Spielende\n");
+			Text textFinish = getNormalText(
+					"Qwixx endet, sobald ein Spieler seinen vierten Fehlwurf angekreuzt hat oder aber der die zweite Reihe beendet wurde.");
+
+			text.getChildren().add(textGoalHeader);
+			text.getChildren().add(textGoal);
+			text.getChildren().add(textAblaufHeader);
+			text.getChildren().add(textAblauf);
+
+			text.getChildren().add(textHintHeader);
+			text.getChildren().add(textHint);
+
+			text.getChildren().add(textFinishHeader);
+			text.getChildren().add(textFinish);
+			JFXDialog dialog = new JFXDialog(getPane(), text, DialogTransition.TOP);
+			dialog.show();
+
+		});
+		btnControls.setOnAction(e -> {
+
+			GridPane pane = new GridPane();
+			pane.setMaxWidth(stage.getWidth() / 1.5);
+			pane.setVgap(40);
+			pane.setHgap(40);
+			pane.setPadding(new Insets(50));
+
+			Text textSelectHeader = getBoldText("Feld ankreuzen:\t\t");
+			Text textSelect = getBoldTextSmall("Linke Maustaste");
+
+			Text textDismissHeader = getBoldText("Würfelpaar\nnicht verwenden:\t");
+			Text textDismiss = getBoldTextSmall("Rechte Maustaste");
+			Text textDismiss2 = getNormalText(" (auf deinem Spielbrett)\n");
+			Text textDismiss3 = getNormalTextSmall("Falls du sowohl die weißen Würfel nicht verwendest " //
+					+ "als auch keinen Farbwürfel, so wird automatisch " //
+					+ "ein Fehlwurf angekreuzt.");
+
+			Text textExitHeader = getBoldText("Spiel verlassen:\t\t");
+			Text textExit = getBoldTextSmall("Taste Esc");
+			Text textExit2 = getNormalText(" (alternativ Schaltfläche oben rechts)");
+
+			TextFlow flowDismiss = new TextFlow(textDismiss, textDismiss2, textDismiss3);
+			TextFlow flowExit = new TextFlow(textExit, textExit2);
+
+			pane.add(textSelectHeader, 0, 0);
+			pane.add(textSelect, 1, 0);
+			pane.add(textDismissHeader, 0, 1);
+			pane.add(flowDismiss, 1, 1);
+			pane.add(textExitHeader, 0, 2);
+			pane.add(flowExit, 1, 2);
+
+			JFXDialog dialog = new JFXDialog(getPane(), pane, DialogTransition.TOP);
+			dialog.show();
+
+		});
 		dialogGameOver.getDialog().setOnDialogClosed(e -> stage.hide());
+	}
+
+	private Text getBoldText(String text) {
+		Text boldText = new Text(text);
+		boldText.setStyle("-fx-font-weight: bold; -fx-font-size: 16pt;");
+		return boldText;
+	}
+
+	private Text getBoldTextSmall(String text) {
+		Text boldText = new Text(text);
+		boldText.setStyle("-fx-font-weight: bold; -fx-font-size: 14pt;");
+		return boldText;
+	}
+
+	private Text getNormalText(String text) {
+		Text normalText = new Text(text);
+		normalText.setStyle("-fx-font-size: 14pt;");
+		return normalText;
+	}
+
+	private Text getNormalTextSmall(String text) {
+		Text normalText = new Text(text);
+		normalText.setStyle("-fx-font-size: 12pt;");
+		return normalText;
 	}
 
 	private void addWidgets() {
@@ -97,6 +213,8 @@ public class GameStage extends AbstractPane<StackPane> implements GameListener {
 		HBox.setHgrow(region, Priority.ALWAYS);
 
 		boxButtons.getChildren().add(btnHelp);
+		boxButtons.getChildren().add(btnControls);
+		boxButtons.getChildren().add(btnShowNotifications);
 		boxButtons.getChildren().add(region);
 		boxButtons.getChildren().add(btnExit);
 
@@ -156,7 +274,7 @@ public class GameStage extends AbstractPane<StackPane> implements GameListener {
 
 	@Override
 	public void nextPlayersTurn(IPlayer nextPlayer) {
-		if (nextPlayer instanceof Human) {
+		if (nextPlayer instanceof Human && btnShowNotifications.isSelected()) {
 			notification.show("Du bist am Zug!\nBitte wähle zuerst aus den weißen, dann aus den farbigen Würfeln.");
 		}
 	}
