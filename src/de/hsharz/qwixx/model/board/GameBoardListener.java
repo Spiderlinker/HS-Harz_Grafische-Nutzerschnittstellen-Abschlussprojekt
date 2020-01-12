f86883eb7ba1aebfdcdc759158b5880a01f41f17
Oliver Lindemann <lindemann.oliver@gmx.de>
2020-01-12 11:24:42 +0100

    JavaDoc Added

--- src/de/hsharz/qwixx/model/board/GameBoardListener.java
+++ src/de/hsharz/qwixx/model/board/GameBoardListener.java
@@ -4,12 +4,34 @@ import de.hsharz.qwixx.model.board.row.Row;
 import de.hsharz.qwixx.model.board.row.field.Field;
 import de.hsharz.qwixx.model.dice.DiceColor;
 
+/**
+ * Dieser Listener dient der Benachrichtigung über angekreuzte Felder,
+ * abgeschlossene Reihen oder angekreuzte Fehlwürfe.
+ * 
+ * @author Oliver Lindemann
+ */
 public interface GameBoardListener {
 
+	/**
+	 * Das Feld {@code fieldToCross} wurde in der Reihe {@code rowToCross}
+	 * angekreuzt.
+	 * 
+	 * @param rowToCross   Reihe, in der das Feld angekreuzt wurde
+	 * @param fieldToCross Feld, das in der Reihe {@code rowToCross} angekreuzt
+	 *                     wurde
+	 */
 	void fieldCrossed(Row rowToCross, Field fieldToCross);
 
+	/**
+	 * Die Reihe der gegebenen Farbe {@code color} wurde geschlossen
+	 * 
+	 * @param color Reihe der Farbe, die abgeschlossen wurde
+	 */
 	void rowFinished(DiceColor color);
-	
+
+	/**
+	 * Ein Fehlwurf wurde angekreuzt
+	 */
 	void missCrossed();
-	
+
 }
--- src/de/hsharz/qwixx/model/board/UserScore.java
+++ src/de/hsharz/qwixx/model/board/UserScore.java
@@ -8,39 +8,96 @@ import java.util.Map;
 
 import de.hsharz.qwixx.model.dice.DiceColor;
 
+/**
+ * Diese Klasse dient zur Speicherung der Punktzahl eines Spielfeldes eines
+ * Spielers. <br>
+ * Hier wird die Punktzahl jeder Reihe nach ihrer Farbe gespeichert und die
+ * Punktzahl der Fehlwürfe.
+ * 
+ * @author Oliver Lindemann
+ */
 public class UserScore {
 
+	/** Punktezahl pro Fehlwurf */
 	public static final int SCORE_PER_MISS = -5;
+
+	/**
+	 * Nicht-veränderbare-Liste mit Punkten pro angekreuztes Feld. <br>
+	 * Die Punkte sind aufsteigend sortiert. Der Index dient hierbei als Anzahl der
+	 * angekreuzten Felder.
+	 */
 	public static final List<Integer> SCORE_PER_CROSS = Collections
 			.unmodifiableList(Arrays.asList(0, 1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 66, 78));
 
+	/**
+	 * Liste beinhaltet die Punkte für die jeweilige Reihe bzw. der Farbe der Reihe.
+	 * Die Map ist nach dem Schema <FarbeDerReihe, PunkteDerReihe> aufgebaut
+	 */
 	private Map<DiceColor, Integer> rowScores = new EnumMap<>(DiceColor.class);
+
+	/** Gesamtpunktezahl für die Fehlwürfe */
 	private int scoreMisses;
 
+	/**
+	 * Erzeugt einen neuen UserScore mit initialem Punktestand von 0 pro Reihe und
+	 * Fehlwürfe
+	 */
 	public UserScore() {
 		rowScores.put(DiceColor.RED, 0);
 		rowScores.put(DiceColor.YELLOW, 0);
 		rowScores.put(DiceColor.GREEN, 0);
 		rowScores.put(DiceColor.BLUE, 0);
+		scoreMisses = 0;
 	}
 
-	public void setScoreOfRow(DiceColor rowColor, int amountRedFieldsCrossed) {
-		rowScores.put(rowColor, SCORE_PER_CROSS.get(amountRedFieldsCrossed));
+	/**
+	 * Setzt den Punktestand der Reihe der gegebenen Farbe {@code rowColor}. Der
+	 * neue Punktestand wird anhand der gegebenen Anzahl angekreuzter Felder
+	 * {@code amountFieldsCrossed} zusammen mit der Punkteliste
+	 * {@link #SCORE_PER_CROSS} ermittelt. <br>
+	 * 
+	 * @param rowColor            Farbe der Reihe, dessen Punktzahl gesetzt werden
+	 *                            soll
+	 * @param amountFieldsCrossed Anzahl der gesetzten Kreuze in der gegebenen Reihe
+	 */
+	public void setScoreOfRow(DiceColor rowColor, int amountFieldsCrossed) {
+		rowScores.put(rowColor, SCORE_PER_CROSS.get(amountFieldsCrossed));
 	}
 
+	/**
+	 * Setzt den Punktestand der Fehlwürfe.
+	 * 
+	 * @param amountMisses Anzahl der Fehlwürfe.
+	 */
 	public void setScoreMisses(int amountMisses) {
 		this.scoreMisses = SCORE_PER_MISS * amountMisses;
 	}
 
+	/**
+	 * Liefert den Punktestand der Reihe der gegebenen Farbe {@code rowColor}. <br>
+	 * Falls keine passende Reihe zu der gegebenen Farbe gefunden werden konnte, so
+	 * wird 0 zurückgeliefert.
+	 * 
+	 * @param rowColor Farbe der Reihe, dessen Punktzahl geholt werden soll
+	 * @return Punktzahl der Reihe zu der gegebenen Farbe, 0 falls keine Reihe
+	 *         gefunden werden konnte
+	 */
 	public int getScoreOfRow(DiceColor rowColor) {
 		Integer scoreOfRow = rowScores.get(rowColor);
 		return scoreOfRow == null ? 0 : scoreOfRow;
 	}
 
+	/**
+	 * @return Liefert die Punktzahl der Fehlwürfe
+	 */
 	public int getScoreMisses() {
 		return scoreMisses;
 	}
 
+	/**
+	 * @return Liefert die komplette Punktzahl. Dies beinhaltet die Punktzahl jeder
+	 *         Reihe und die Punktzahl der Fehlwürfe
+	 */
 	public int getScoreComplete() {
 		return rowScores.values().stream()//
 				.mapToInt(i -> i) //
