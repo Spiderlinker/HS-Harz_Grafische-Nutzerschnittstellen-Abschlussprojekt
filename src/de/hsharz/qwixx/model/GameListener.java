5c3d2dac0ae478a56a5ddec64689aaebc1873f34
Spiderlinker <support@spiderlinker.de>
2020-01-13 13:06:48 +0100

    JavaDoc hinugefuegt

--- src/de/hsharz/qwixx/model/GameListener.java
+++ src/de/hsharz/qwixx/model/GameListener.java
@@ -2,11 +2,36 @@ package de.hsharz.qwixx.model;
 
 import de.hsharz.qwixx.model.player.IPlayer;
 
+/**
+ * Diese Klasse dient der Benachrichtigung über verschiedene Abläufe während
+ * eines Spiels. Diese Klasse stellt Methoden bereit, die über den nächsten
+ * Spieler an der Reihe ({@link #nextPlayersTurn(IPlayer)}), ungültige Spielzüge
+ * ({@link #invalidDiceChoiceMade(IPlayer, String)} und das Spielende
+ * ({@link #gameOver()}) benachrichtigen.
+ * 
+ * @author Oliver Lindemann
+ */
 public interface GameListener {
 
+	/**
+	 * Der gegebene Spieler als Parameter {@code nextPlayer} ist jetzt an der Reihe.
+	 * 
+	 * @param nextPlayer Dieser Spieler ist am Zug
+	 */
 	void nextPlayersTurn(IPlayer nextPlayer);
 
+	/**
+	 * Der als Parameter gegebene Spieler {@code player} hat einen ungültigen
+	 * Spielzug gemacht. Die Beschreibung des Fehlers bzw. des ungültigen Spielzuges
+	 * ist in der Nachricht {@code msg} erläutert.
+	 * 
+	 * @param player Spieler, der einen ungültigen Spielzug gemacht hat
+	 * @param msg    Beschreibung des ungültigen Spielzuges
+	 */
 	void invalidDiceChoiceMade(IPlayer player, String msg);
 
+	/**
+	 * Das Spiel ist vorbei
+	 */
 	void gameOver();
 }
