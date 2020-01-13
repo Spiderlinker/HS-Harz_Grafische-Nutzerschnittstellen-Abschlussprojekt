e827d06628a950bf9a66752b5f85bff81fc24ce7
Spiderlinker <support@spiderlinker.de>
2020-01-13 18:35:39 +0100

    Weiter-Button in Spieler-GameboardUI eingefuegt

--- src/de/hsharz/qwixx/ui/game/board/GameBoardSimple.java
+++ src/de/hsharz/qwixx/ui/game/board/GameBoardSimple.java
@@ -1,16 +1,37 @@
 package de.hsharz.qwixx.ui.game.board;
 
+import java.io.File;
 import java.util.List;
+
+import com.jfoenix.controls.JFXButton;
 
 import de.hsharz.qwixx.model.dice.DicePair;
 import de.hsharz.qwixx.model.player.DiceSelectionType;
 import de.hsharz.qwixx.model.player.IPlayer;
 import de.hsharz.qwixx.ui.game.board.row.RowUI;
+import javafx.geometry.Pos;
+import javafx.scene.control.Tooltip;
+import javafx.scene.image.ImageView;
 
 public class GameBoardSimple extends GameBoardUI {
 
 	public GameBoardSimple(IPlayer player) {
 		super(player);
+		createNextButton();
+	}
+
+	private void createNextButton() {
+		JFXButton btnNext = new JFXButton("", new ImageView(new File("images/arrow_right.png").toURI().toString()));
+		btnNext.setFocusTraversable(false);
+
+		Tooltip btnNextTooltip = new Tooltip("Weiter zum nächsten Würfelpaar / Zug");
+		btnNextTooltip.setStyle("-fx-font-size: 14pt");
+		btnNext.setTooltip(btnNextTooltip);
+		
+		btnNext.setOnAction(e -> playerSelectedDice(DicePair.EMPTY));
+
+		btnNext.setAlignment(Pos.CENTER);
+		boxGameBoardHeader.getChildren().add(btnNext);
 	}
 
 	@Override
