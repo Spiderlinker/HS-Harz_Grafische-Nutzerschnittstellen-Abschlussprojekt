5199529e6e24020edb1dbb7be3cc22d2d0b5b6db
Spiderlinker <support@spiderlinker.de>
2020-02-06 16:45:29 +0100

    Abgeschlossene Reihe wird auch bei Computer durchgestrichen

--- src/de/hsharz/qwixx/ui/game/board/ComputerGameBoardUI.java
+++ src/de/hsharz/qwixx/ui/game/board/ComputerGameBoardUI.java
@@ -1,6 +1,7 @@
 package de.hsharz.qwixx.ui.game.board;
 
 import de.hsharz.qwixx.model.player.IPlayer;
+import de.hsharz.qwixx.ui.game.board.row.RowUI;
 
 public class ComputerGameBoardUI extends GameBoardUI {
 
@@ -13,6 +14,20 @@ public class ComputerGameBoardUI extends GameBoardUI {
 
 	private void disableMissFields() {
 		scoreLegend.getMissFields().forEach(missField -> missField.setDisabled(true));
+	}
+
+	@Override
+	public void nextPlayersTurn(IPlayer nextPlayer) {
+		super.nextPlayersTurn(nextPlayer);
+		checkClosedRows();
+	}
+
+	private void checkClosedRows() {
+		for (RowUI row : rows.values()) {
+			if (player.getGameBoard().getRowClosedSupplier().isRowClosed(row.getRow().getColor())) {
+				row.setStroked(true);
+			}
+		}
 	}
 
 }
