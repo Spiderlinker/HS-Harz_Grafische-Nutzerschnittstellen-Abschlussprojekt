1142596b504c42c6958ced00d15caedebd1918a5
Spiderlinker <support@spiderlinker.de>
2020-02-07 15:09:00 +0100

    Abgeschlossene Reihen werden von Vaterklasse gestrichen

--- src/de/hsharz/qwixx/ui/game/board/ComputerGameBoardUI.java
+++ src/de/hsharz/qwixx/ui/game/board/ComputerGameBoardUI.java
@@ -1,7 +1,6 @@
 package de.hsharz.qwixx.ui.game.board;
 
 import de.hsharz.qwixx.model.player.IPlayer;
-import de.hsharz.qwixx.ui.game.board.row.RowUI;
 
 public class ComputerGameBoardUI extends GameBoardUI {
 
@@ -14,20 +13,6 @@ public class ComputerGameBoardUI extends GameBoardUI {
 
 	private void disableMissFields() {
 		scoreLegend.getMissFields().forEach(missField -> missField.setDisabled(true));
-	}
-
-	@Override
-	public void nextPlayersTurn(IPlayer nextPlayer) {
-		super.nextPlayersTurn(nextPlayer);
-		checkClosedRows();
-	}
-
-	private void checkClosedRows() {
-		for (RowUI row : rows.values()) {
-			if (player.getGameBoard().getRowClosedSupplier().isRowClosed(row.getRow().getColor())) {
-				row.setStroked(true);
-			}
-		}
 	}
 
 }
